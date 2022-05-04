76cf271dd636fdf73cff5a9b235fd1d65cdc77c1
mcaglayan <murat.caglayan@live.com>
2022-05-04 22:06:11 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Running on port:${PORT}`));
+
+app.use(fonts({
+  'csspath': '/css',
+  'fontspath': '/fonts',
+  'fontsdir': './fonts'
+}));
+
+const groceriesList = [
+  {
+    item: "milk",
+    quantity: 4,
+  },
+  {
+    item: "cereal",
+    quantity: 6,
+  },
+  {
+    item: "pop-tart",
+    quantity: 7,
+  },
+];
+
+app.get("/groceries", (req, res) => {
+  res.send(groceriesList);
+});
+
+app.post("/groceries", (req, res) => {
+  groceriesList.push(req.body)
+  res.sendStatus(201);
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/groceries");
+});
+
+app.post('/groceries', (req,res) =>{
+
+
+})
